c17dfc0853f783f5fb7c9f8524071271013b82f6
kencast <kencastillo@estudiantec.cr>
2024-10-04 20:11:37 -0600

    Cambios

--- algorithms/data-st/sparseTFast.cpp
+++ algorithms/data-st/sparseTFast.cpp
@@ -9,36 +9,38 @@ typedef long long ll;
 // pre C++20
 int log2_floor(unsigned long long i)
 {
-    return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
+	return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
 }
 
 int n = 10;
 int lg = log2_floor(n);
-vector<vll> st(lg+1, vll(n));
+vector<vll> st(lg + 1, vll(n));
 
-ll f(ll a, ll b){
+ll f(ll a, ll b)
+{
 	return min(a, b);
 }
 
 void build()
 {
-    for (int i = 1; i <= lg; i++){
-	    for(int j=0; j+(1<<i)<=n; j++)
-		    st[i][j]=f(st[i-1][j], st[i-1][j+(1<<(i-1))]);
-
-    }
-
+	for (int i = 1; i <= lg; i++)
+	{
+		for (int j = 0; j + (1 << i) <= n; j++)
+			st[i][j] = f(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+	}
 }
 
-ll query(int l, int r){ // [l, r]
-	int i=log2_floor(r-l+1);
-	return min(st[i][l], st[i][r-(1<<i)+1]);
+ll query(int l, int r)
+{ // [l, r]
+	int i = log2_floor(r - l + 1);
+	return min(st[i][l], st[i][r - (1 << i) + 1]);
 }
 
 int main(int argc, char const *argv[])
 {
-	for(int i=0; i<10; i++) st[0][i]=i+1;
+	for (int i = 0; i < 10; i++)
+		st[0][i] = i + 1;
 	build();
-	cout<<query(3, 8)<<'\n';
+	cout << query(3, 8) << '\n';
 	return 0;
 }
--- work/a.cpp
+++ work/a.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <iomanip>
 using namespace std;
 
 #define uf(i, a, b) for (int i = (a); i < (b); i++)
@@ -15,7 +16,28 @@ typedef vector<long long> vll;
 typedef list<int> li;
 
 void solve(){
-
+  int n;
+  cin>>n;
+  double loc[n];
+  double vel[n];
+  uf(i, 0, n) cin>>loc[i]>>vel[i];
+  double inf=0, sup=2e9, m;
+  double error=0.0000001;
+  auto eval=[&](double time){
+    double l=loc[0]-time*vel[0], r=loc[0]+time*vel[0];
+    uf(i, 0, n){
+      l=max(l, loc[i]-time*vel[i]), r=min(r, loc[i]+time*vel[i]);
+    }
+    return r>=l;
+  };
+  while(inf+error<sup){
+    m=inf+(sup-inf)/2.0;
+    if(eval(m)) sup=m;
+    else inf=m;
+  }
+  if(eval(inf)) sup=inf;
+  cout<<setprecision(7);
+  cln(sup);
 }
 
 int main()
@@ -23,7 +45,6 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int t=1;
-    cin >> t;
     while (t--)
     {
 	    solve();
